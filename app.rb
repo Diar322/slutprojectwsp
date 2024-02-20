7d6bd7e266c15f01ff57b1dba2054ba9979416d7
Diar322 <68732081+Diar322@users.noreply.github.com>
2024-02-20 10:14:07 +0100

    put

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
+require 'bcrypt'
 require_relative 'model.rb'
 
 enable :sessions
@@ -15,7 +16,7 @@ get('/browse') do
     db = SQLite3::Database.new("db/databas.db")
     db.results_as_hash = true
 
-    @result = db.execute("SELECT * FROM product")
+    @result = db.execute("SELECT * FROM products")
 
     slim(:"browse/index")
 end
@@ -28,7 +29,7 @@ get('/browse/:id') do
     id = params[:id].to_i
     db = SQLite3::Database.new("db/databas.db")
     db.results_as_hash = true
-    @result = db.execute("SELECT * FROM product WHERE id = ?",id).first
+    @result = db.execute("SELECT * FROM products WHERE id = ?",id).first
     slim(:"browse/show")
 end
 
@@ -41,7 +42,7 @@ post('/browse/new') do
 
   
     db = SQLite3::Database.new("db/databas.db")
-    db.execute("INSERT INTO product (name, description, price, file_path) VALUES (?, ?, ?, ?)", title, desc, price, file_path)
+    db.execute("INSERT INTO products (name, description, price, file_path) VALUES (?, ?, ?, ?)", title, desc, price, file_path)
 
     path = File.join("public/sheet_music/",file_path)
     File.write(path, File.read(params[:file][:tempfile]))
@@ -53,16 +54,47 @@ get('/register') do
     slim(:register)
 end
 
-get('/login') do
-    slim(:login)
+post('/registration_result') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+  
+    if (password == password_confirm)
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('db/databas.db')
+      db.execute("INSERT INTO users (username, pwdigest, role) VALUES (?,?,?)", username, password_digest, 0)
+      redirect('/')
+    else
+      #fel
+      "Lösenorden matchade inte!"
+    end
 end
 
 post('login_result') do
-    session[:user] = params[:user]
-    session[:pwd] = params[:pwd] 
-
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('db/todo2021.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+  
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+  
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id
+      redirect('/todos')
+    else
+      "FEL LÖSEN"
+    end
     redirect('/')
 end
+
+get('/login') do
+    #login som gör cooldown för A nivå
+    slim(:login)
+end
+
+
 
 get('/download/:file_path') do
     file_path = "sheet_music/#{params[:file_path]}"
