0c2bc98191d98b9a14f1d81ddf4626fd5f3c27d7
Diar322 <Diar322>
2024-04-11 19:14:53 +0200

    detta fungerar itne

--- app.rb
+++ app.rb
@@ -2,23 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
-require 'bcrypt'
 require_relative 'model.rb'
 
 enable :sessions
 
-
 get('/')  do
   slim(:start)
-end 
+end
 
-get('/browse') do
+get('/browse/') do
   @result = fetch_products()
   slim(:"products/index")
 end
 
 get('/browse/new') do
-  if session[:id] == nil
+  if session[:user_id] == nil
     redirect('/login')
   end
   slim(:"products/new")
@@ -27,46 +25,59 @@ end
 get('/browse/:id') do
   id = params[:id].to_i
   @result = fetch_product(id)
-  slim(:"products/show") 
+  slim(:"products/show")
 end
 
 
-post('/browse/new') do
+post('/browse') do
   title = params[:title]
   desc = params[:description]
   price = params[:price].to_i
   if params[:file] != nil
     file_path = params[:file][:filename]
     path = File.join("public/sheet_music/",file_path)
-    File.write(path, File.read(params[:file][:tempfile]))  
+    File.write(path, File.read(params[:file][:tempfile]))
   end
 
-  insert_product(title, desc, price, file_path)
-
-  
-  redirect('/browse')
+  insert_product(title, desc, price, file_path, session["user_id"])
+  redirect('/browse/')
 end
+
+post('/browse/:id/add') do
+  id = params[:id]
+  insert_purchase(session[:user_id], id.to_i)
+  p session[:user_id]
+  p id
+  redirect('/browse/')
+end
+
+post('/browse/:id/remove') do
+  id = params[:id]
+  remove_purchase(id)
+  redirect('/browse/')
+end
+
+
+# before('/browse/:id/*') do
+#   id = params[:id].to_i
+#   product = fetch_product(id)
+#   if session["user_id"] != product["user_id"]
+#     redirect('/')
+#     "Du har inte åtkomst till denna produkt"
+#   end
+# end
+
 
 post('/browse/:id/delete') do
   id = params[:id].to_i
   delete_product(id)
-  redirect('/browse')
+  redirect('/browse/')
 end
 
-
 get('/browse/:id/edit') do
-  id = params[:id]
-  db = SQLite3::Database.new('db/databas.db')
-  db.results_as_hash = true
-  @result = db.execute("SELECT * FROM products WHERE id = ?", id).first
-  p @result
-  #if @result["user_id"] == session[:id]
-    # rätt personen är inloggad
+  id = params[:id].to_i
+  @result = fetch_product(id)
   slim(:"products/edit")
-  #else
-    # fel person är inloggad
-    #"Logga in först"
-  #end
 end
 
 post('/browse/:id/update') do
@@ -74,20 +85,10 @@ post('/browse/:id/update') do
   desc = params[:description]
   price = params[:price]
   id = params[:id]
-  db = SQLite3::Database.new('db/databas.db')
-  db.execute("UPDATE products SET name=?,description=?,price=? WHERE id=?", name, desc, price, id)
 
-  redirect('/browse')
+  edit_product(name, desc, price, id)
+  redirect('/browse/')
 end
-
-post('/browse/:id/add') do
-
-  db = SQLite3::Database.new('db/databas.db')
-  db.execute("INSERT INTO user_product_rel (user_id, product_id) VALUES (?, ?)", )
-  redirect('/browse')
-end
-
-  
 
 get('/register') do
     slim(:register)
@@ -97,31 +98,27 @@ post('/registration_result') do
     username = params[:username]
     password = params[:password]
     password_confirm = params[:password_confirm]
-  
-    if (password == password_confirm)
-      password_digest = BCrypt::Password.create(password)
-      db = SQLite3::Database.new('db/databas.db')
-      db.execute("INSERT INTO users (username, pwdigest, role) VALUES (?,?,?)", username, password_digest, 0)
+
+    if register_user(username, password, password_confirm)
       redirect('/')
     else
-      #fel
-      "Lösenorden matchade inte!"
+      return register_user(username, password, password_confirm)
     end
 end
 
 post('/login_result') do
     username = params[:user]
     password = params[:pwd]
-    db = SQLite3::Database.new('db/databas.db')
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM users WHERE username=?", username).first
-    #result = db.execute("SELECT * FROM users WHERE username = ?", username).first
-  
+
+    if password == nil || username == nil
+      redirect('/login')
+    end
+
+    result = fetch_user(username)
     pwdigest = result["pwdigest"]
-    id = result["id"]
-  
     if BCrypt::Password.new(pwdigest) == password
-      session[:id] = id
+      session[:user_id] = result["id"]
+      session[:role] = result["role"]
       redirect('/')
     else
       "FEL LÖSEN"
@@ -137,4 +134,10 @@ end
 get('/logout') do
   session.clear()
   redirect('/')
+end
+
+get('/purchases') do
+  @result = fetch_purchased_products(session[:user_id])
+  p fetch_purchased_products(session[:user_id])
+  slim(:purchases)
 end
--- model.rb
+++ model.rb
@@ -1,3 +1,6 @@
+require 'bcrypt'
+require 'sinatra/flash'
+
 def connect_to_db()
     db = SQLite3::Database.new("db/databas.db")
     db.results_as_hash = true
@@ -14,12 +17,76 @@ def fetch_product(id)
     return db.execute("SELECT * FROM products WHERE id = ?", id).first
 end
 
-def insert_product(title, desc, price, file_path)
+def fetch_purchased_products(user_id)
     db = connect_to_db()
-    db.execute("INSERT INTO products (name, description, price, file_path) VALUES (?, ?, ?, ?)", title, desc, price, file_path)
+    p user_id
+    return db.execute("SELECT * FROM products INNER JOIN user_product_rel ON products.id = user_product_rel.product_id WHERE user_id = ?", user_id)
+end
+
+def insert_product(title, desc, price, file_path, user_id)
+    db = connect_to_db()
+    if title == nil || desc == nil|| price == nil || file_path == nil
+        flash[:notice] = "rutorna kan ej vara tomma"
+    elsif !(price.integer?)
+        flash[:notice] = "priset måste vara en tal"
+    elsif price.to_i > 9999
+        flash[:notice] = "priset kan ej vara över 9999"
+    else
+        db.execute("INSERT INTO products (name, description, price, file_path, created_by) VALUES (?, ?, ?, ?, ?)", title, desc, price, file_path, user_id)
+        redirect('/browse/')
+        flash[:notice] = "produkt adderad"
+    end
+    redirect('/browse/new')
 end
 
 def delete_product(id)
     db = connect_to_db()
     db.execute("DELETE FROM products WHERE id = ?", id)
+    flash[:notice] = "produkt raderad"
+end
+
+def edit_product(name, desc, price, id)
+    db = connect_to_db()
+    db.execute("UPDATE products SET name=?,description=?,price=? WHERE id=?", name, desc, price, id)
+    flash[:notice] = "produkt redigerad"
+end
+
+def register_user(username, password, password_confirm)
+    db = connect_to_db()
+
+    p username, password, password_confirm
+
+    if password != password_confirm
+        flash[:notice] = "inte samma lösenord"
+    elsif password.length > 8
+        flash[:notice] = "lösenord måste vara längre än 8!"
+    elsif password != /[A-Z]/
+        flash[:notice] = "LÖSENORD MÅSTE HA EN STOR BOKSTAV"
+    elsif password != /[0-9]/
+        flash[:notice] = "L0S3N 03D MÅ573 H2 3N 519931"
+    elsif fetch_user(username) != nil
+        flash[:notice] = "användarnmanet används redan"
+    else
+        password_digest = BCrypt::Password.create(password)
+        db.execute("INSERT INTO users (username, pwdigest, role) VALUES (?,?,?)", username, password_digest, 0)
+        redirect('/')
+    end
+    redirect('/register')
+end
+
+def fetch_user(username)
+    db = connect_to_db()
+    return db.execute("SELECT * FROM users WHERE username=?", username).first
+end
+
+def insert_purchase(user_id, product_id)
+    db = connect_to_db()
+    db.execute("INSERT INTO user_product_rel (user_id, product_id) VALUES (?, ?)", user_id,  product_id)
+    flash[:notice] = "produkt köpt"
+end
+
+def remove_purchase(id)
+    db = connect_to_db()
+    db.execute("DELETE FROM user_product_rel WHERE id = ?", id)
+    flash[:notice] = "produkt borttagen från purchases"
 end
