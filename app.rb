e259ce7741e223bd19abcbe93157ba24f5339fb1
Diar322 <Diar322>
2024-04-29 14:40:31 +0200

    Lade till login-cooldown och yardoc

--- app.rb
+++ app.rb
@@ -6,15 +6,24 @@ require_relative 'model.rb'
 
 enable :sessions
 
+include Model
+
+# Displays landing page
+#
 get('/')  do
   slim(:start)
 end
 
+# Displays all products
+#
+#@see Model#fetch_products
 get('/browse/') do
   @result = fetch_products()
   slim(:"products/index")
 end
 
+# Display product creation page
+#
 get('/protected/browse/new') do
   if session[:user_id] == nil
     redirect('/login')
@@ -22,13 +31,24 @@ get('/protected/browse/new') do
   slim(:"products/new")
 end
 
+# Displays a single Product
+#
+# @param [Integer] :id, the ID of the product
+# @see Model#fetch_product
 get('/browse/:id') do
   id = params[:id].to_i
   @result = fetch_product(id)
   slim(:"products/show")
 end
 
-
+# Creates a new product and redirects to '/browse/'
+#
+# @param [String] title, The title of the product
+# @param [String] desc, The description of the product
+# @param [String] price, The price of the product
+# @param [String] price, The file_path of the product
+#
+# @see Model#insert_product
 post('/browse') do
   title = params[:title]
   desc = params[:description]
@@ -43,30 +63,57 @@ post('/browse') do
   redirect('/browse/')
 end
 
+# Creates a new purchase and redirects to '/browse/'
+#
+# @param [Integer] :id, the ID of the product
+#
+# @see Model#insert_purchase
 post('/protected/browse/:id/add') do
   id = params[:id]
   insert_purchase(session[:user_id], id.to_i)
   redirect('/browse/')
 end
 
+# Removes a purchase and redirects to '/browse/'
+#
+# @param [Integer] :id, the ID of the product
+#
+# @see Model#remove_purchase
 post('/protected/browse/:id/remove') do
   id = params[:id]
   remove_purchase(id)
   redirect('/browse/')
 end
 
+# Deletes a product and redirects to '/browse/'
+#
+# @param [Integer] :id, the ID of the product
+#
+# @see Model#delete_product
 post('/private/browse/:id/delete') do
   id = params[:id].to_i
   delete_product(id)
   redirect('/browse/')
 end
 
+# Displays the edit menu for a product
+#
+# @param [Integer] :id, the ID of the product
+# @see Model#fetch_product
 get('/private/browse/:id/edit') do
   id = params[:id].to_i
   @result = fetch_product(id)
   slim(:"products/edit")
 end
 
+# Updates an existing product and redirects to '/browse/'
+#
+# @param [String] name, The new title of the product
+# @param [String] desc, The new description of the product
+# @param [String] price, The new price of the product
+# @param [Integer] :id, The id of the product
+#
+# @see Model#edit_product
 post('/private/browse/:id/update') do
   name = params[:name]
   desc = params[:description]
@@ -77,10 +124,19 @@ post('/private/browse/:id/update') do
   redirect('/browse/')
 end
 
+# Displays the user registration page
+#
 get('/register') do
     slim(:"users/register")
 end
 
+# Attempts registration
+#
+# @param [String] username, The username
+# @param [String] password, The password
+# @param [String] password_confirm, The repeated password
+#
+# @see Model#register_user
 post('/registration_result') do
     username = params[:username]
     password = params[:password]
@@ -93,6 +149,12 @@ post('/registration_result') do
     end
 end
 
+# Attempts login and updates the session
+#
+# @param [String] username, The username
+# @param [String] password, The password
+#
+# @see Model#login
 post('/login_result') do
     username = params[:user]
     password = params[:pwd]
@@ -108,22 +170,28 @@ post('/login_result') do
 
 end
 
+# Displays the login page
+#
 get('/login') do
-    #login som gör cooldown för A nivå
     slim(:"users/login")
 end
 
+# Logs the user out, clears session, and redirects to '/'
+#
 get('/logout') do
   session.clear()
   redirect('/')
 end
 
+# Displays purchases
+#
+# @param [Integer] :user_id, The id of the user
+# @see Modepurchasel#fetch_purchased_products
 get('/protected/purchases') do
   @result = fetch_purchased_products(session[:user_id])
   p fetch_purchased_products(session[:user_id])
   slim(:purchases)
 end
-
 
 before('/protected/*') do
   if session[:user_id] == nil
@@ -145,17 +213,31 @@ before('/admin/*') do
   end
 end
 
+# Display list of users
+#
 get('/admin/users/') do
   @result = fetch_users()
   slim(:"users/index")
 end
 
+# Displays the edit menu for a user
+#
+# @param [Integer] :id, The id of the product
+# @see Model#fetch_user_id
 get('/admin/users/:id/edit') do
   id = params[:id].to_i
   @result = fetch_user_id(id)
   slim(:"users/edit")
 end
 
+# Updates an existing user and redirects to '/admin/users/'
+#
+# @param [String] name, The new username of the user
+# @param [String] pwd, The new password of the user
+# @param [String] role, The new role of the user
+# @param [Integer] :id, The id of the user
+#
+# @see Model#edit_user
 post('/admin/users/:id/update') do
   name = params[:name]
   pwd = params[:pwd]
@@ -166,10 +248,13 @@ post('/admin/users/:id/update') do
   redirect('/admin/users/')
 end
 
+# Deletes an existing user
+#
+# @param [Integer] :id, The id of the user
+#
+# @see Model#delete_user
 post('/admin/users/:id/delete') do
   id = params[:id].to_i
   delete_user(id)
   redirect('/admin/users/')
 end
-
-#Du måste fixa inlogg så att det tar tid att logga in, också kolla om redirect skall finnas i model.rb
--- model.rb
+++ model.rb
@@ -1,159 +1,243 @@
 require 'bcrypt'
 require 'sinatra/flash'
 
-$attempts = {}
+module Model
+    $attempts = {}
 
-def connect_to_db()
-    db = SQLite3::Database.new("db/databas.db")
-    db.results_as_hash = true
-    return db
-end
-
-def fetch_products()
-    db = connect_to_db()
-    return db.execute("SELECT * FROM products")
-end
-
-def fetch_product(id)
-    db = connect_to_db()
-    return db.execute("SELECT * FROM products WHERE id = ?", id).first
-end
-
-def fetch_purchased_products(user_id)
-    db = connect_to_db()
-    return db.execute("SELECT * FROM products INNER JOIN user_product_rel ON products.id = user_product_rel.product_id WHERE user_id = ?", user_id)
-end
-
-def insert_product(title, desc, price, file_path, user_id)
-    db = connect_to_db()
-    if title == nil || desc == nil|| price == nil || file_path == nil
-        flash[:notice] = "rutorna kan ej vara tomma"
-    elsif !(price.integer?)
-        flash[:notice] = "priset måste vara en tal"
-    elsif price.to_i > 9999
-        flash[:notice] = "priset kan ej vara över 9999"
-    else
-        db.execute("INSERT INTO products (name, description, price, file_path, created_by) VALUES (?, ?, ?, ?, ?)", title, desc, price, file_path, user_id)
-        redirect('/browse/')
-        flash[:notice] = "produkt adderad"
-    end
-    redirect('/browse/new')
-end
-
-def delete_product(id)
-    db = connect_to_db()
-    db.execute("DELETE FROM products WHERE id = ?", id)
-    db.execute("DELETE FROM user_product_rel WHERE product_id = ?", id)
-    flash[:notice] = "produkt raderad"
-end
-
-def edit_product(name, desc, price, id)
-    db = connect_to_db()
-    db.execute("UPDATE products SET name=?,description=?,price=? WHERE id=?", name, desc, price, id)
-    flash[:notice] = "produkt redigerad"
-end
-
-def register_user(username, password, password_confirm)
-    db = connect_to_db()
-
-    if password != password_confirm
-        flash[:notice] = "inte samma lösenord"
-    elsif password.length > 8
-        flash[:notice] = "lösenord måste vara längre än 8!"
-    elsif password != /[A-Z]/
-        flash[:notice] = "LÖSENORD MÅSTE HA EN STOR BOKSTAV"
-    elsif password != /[0-9]/
-        flash[:notice] = "L0S3N 03D MÅ573 H2 3N 519931"
-    elsif fetch_user(username) != nil
-        flash[:notice] = "användarnmanet används redan"
-    else
-        password_digest = BCrypt::Password.create(password)
-        db.execute("INSERT INTO users (username, pwdigest, role) VALUES (?,?,?)", username, password_digest, 0)
-        redirect('/')
-    end
-    redirect('/register')
-end
-
-def login(username, password)
-    ip = request.ip
-
-    if $attempts[ip] == nil
-        #fine
-    elsif ($attempts[ip] - Time.now).to_i > 5
-        flash[:notice] = "Loggar in för snabbt!"
-        redirect('login')
+    # Connects to db
+    #
+    # @return [SQLite3::Database] containing everything as hashes
+    def connect_to_db()
+        db = SQLite3::Database.new("db/databas.db")
+        db.results_as_hash = true
+        return db
     end
 
-    if password == nil || username == nil
-        flash[:notice] = "Inga tomma värden!"
-        redirect('/login')
+    # Fetches all products
+    #
+    # @return [Array] containing all products and their data as hashes
+    def fetch_products()
+        db = connect_to_db()
+        return db.execute("SELECT * FROM products")
     end
-    result = fetch_user(username)
 
-    if result == nil
-        flash[:notice] = "Loggade in!"
+    # Fetch specific product
+    #
+    # @param [Integer] id, the ID of the product
+    # @return [Hash] containing the product
+    def fetch_product(id)
+        db = connect_to_db()
+        return db.execute("SELECT * FROM products WHERE id = ?", id).first
     end
-    pwdigest = result["pwdigest"]
-    if BCrypt::Password.new(pwdigest) == password
-        flash[:notice] = "Loggade in!"
-        return true
-    else
-        $attempts[ip] = Time.now
-        flash[:notice] = "Fel lösenord!"
-        return false
+
+    # Fetch purchased products for user
+    #
+    # @param [Integer] user_id, the ID of the product
+    # @return [Array] containing the purchased products as hashes
+    def fetch_purchased_products(user_id)
+        db = connect_to_db()
+        return db.execute("SELECT * FROM products INNER JOIN user_product_rel ON products.id = user_product_rel.product_id WHERE user_id = ?", user_id)
     end
+
+    # Attempts to add new row to products table
+    #
+    # @param [String] title, the title of the product
+    # @param [String] desc, the desc of the product
+    # @param [String] price, the price of the product
+    # @param [String] filepath, the file_path of the product
+    # @param [String] user_id, the user_id of the product user who created the product
+    # @return [flash] success message
+    def insert_product(title, desc, price, file_path, user_id)
+        db = connect_to_db()
+        if title == nil || desc == nil|| price == nil || file_path == nil
+            flash[:notice] = "rutorna kan ej vara tomma"
+        elsif !(price.integer?)
+            flash[:notice] = "priset måste vara en tal"
+        elsif price.to_i > 9999
+            flash[:notice] = "priset kan ej vara över 9999"
+        else
+            db.execute("INSERT INTO products (name, description, price, file_path, created_by) VALUES (?, ?, ?, ?, ?)", title, desc, price, file_path, user_id)
+            redirect('/browse/')
+            flash[:notice] = "produkt adderad"
+        end
+        redirect('/browse/new')
+    end
+
+
+    # Attempts to delete a row from the articles table
+    #
+    # @param [Integer] article_id The article's ID
+    # @param [Hash] params form data
+    # @option params [String] title The title of the article
+    # @option params [String] content The content of the article
+    #
+    # @return [Hash]
+    #   * :error [Boolean] whether an error occured
+    #   * :message [String] the error message
+    def delete_product(id)
+        db = connect_to_db()
+        db.execute("DELETE FROM products WHERE id = ?", id)
+        db.execute("DELETE FROM user_product_rel WHERE product_id = ?", id)
+        flash[:notice] = "produkt raderad"
+    end
+
+    # Attempts to update a row in the products table
+    #
+    # @param [Integer] id The product's ID
+    # @param [Integer] price, The price of the product
+    # @option params [String] name, The title of the product
+    # @option params [String] desc, The content of the product
+    #
+    # @return [flash] Success message
+    def edit_product(name, desc, price, id)
+        db = connect_to_db()
+        db.execute("UPDATE products SET name=?,description=?,price=? WHERE id=?", name, desc, price, id)
+        flash[:notice] = "produkt redigerad"
+    end
+
+    # Attempts to add new row to users table
+    #
+    # @param [String] username, the username of the user
+    # @param [String] password, the password of the user
+    # @param [String] password_confirm, the password_confirmed of the user
+    # @return [flash] if error
+    def register_user(username, password, password_confirm)
+        db = connect_to_db()
+
+        if password != password_confirm
+            flash[:notice] = "inte samma lösenord"
+        elsif password.length > 8
+            flash[:notice] = "lösenord måste vara längre än 8!"
+        elsif password != /[A-Z]/
+            flash[:notice] = "LÖSENORD MÅSTE HA EN STOR BOKSTAV"
+        elsif password != /[0-9]/
+            flash[:notice] = "L0S3N 03D MÅ573 H2 3N 519931"
+        elsif fetch_user(username) != nil
+            flash[:notice] = "användarnmanet används redan"
+        else
+            password_digest = BCrypt::Password.create(password)
+            db.execute("INSERT INTO users (username, pwdigest, role) VALUES (?,?,?)", username, password_digest, 0)
+            redirect('/')
+        end
+        redirect('/register')
+    end
+
+    # Attempts to login
+    #
+    # @option params [String] username, The username
+    # @option params [String] password, The password
+    #
+    # @return [true] if credentials match a user
+    # @return [false] if credentials do not match a user
+    def login(username, password)
+        ip = request.ip
+
+        if $attempts[ip] == nil
+            #fine
+        elsif (Time.now - $attempts[ip]).to_i < 10
+            flash[:notice] = "Loggar in för snabbt!"
+            return false
+        end
+
+        if password == nil || username == nil
+            flash[:notice] = "Inga tomma värden!"
+            return false
+        end
+        result = fetch_user(username)
+
+        if result == nil
+            flash[:notice] = "Inget konto med det användarnamnet!"
+            return false
+        end
+
+        pwdigest = result["pwdigest"]
+        if BCrypt::Password.new(pwdigest) == password
+            flash[:notice] = "Loggade in!"
+            return true
+        else
+            $attempts[ip] = Time.now
+            flash[:notice] = "Fel lösenord!"
+            return false
+        end
+    end
+
+    # Searches for user with username
+    #
+    # @param [String] username
+    #
+    # @return [Hash] returns the user
+    def fetch_user(username)
+        db = connect_to_db()
+        return db.execute("SELECT * FROM users WHERE username=?", username).first
+    end
+
+    # Searches for user with id
+    #
+    # @param [Integer] id
+    #
+    # @return [Hash] returns the user
+    def fetch_user_id(id)
+        db = connect_to_db()
+        return db.execute("SELECT * FROM users WHERE id = ?", id).first
+    end
+
+    # Searches for user with id
+    #
+    # @return [Array] returns all users as hashes
+    def fetch_users()
+        db = connect_to_db()
+        return db.execute("SELECT * FROM users")
+    end
+
+    # Adds a purchase
+    #
+    # @param [Integer] user_id, the users id
+    # @param [Integer] product_id, the products id
+    #
+    # @return [flash] message confirming purchase
+    def insert_purchase(user_id, product_id)
+        db = connect_to_db()
+        db.execute("INSERT INTO user_product_rel (user_id, product_id) VALUES (?, ?)", user_id,  product_id)
+        flash[:notice] = "produkt köpt"
+    end
+
+    # Deletes a pruchase
+    #
+    # @param [Integer] id, the purchase id
+    #
+    # @return [flash] message confirming deletion
+    def remove_purchase(id)
+        db = connect_to_db()
+        db.execute("DELETE FROM user_product_rel WHERE id = ?", id)
+        flash[:notice] = "produkt borttagen från purchases"
+    end
+
+    # Deletes a user
+    #
+    # @param [Integer] id, the user id
+    #
+    # @return [flash] message confirming deletion
+    def delete_user(id)
+        db = connect_to_db()
+        db.execute("DELETE FROM users WHERE id = ?", id)
+        db.execute("DELETE FROM user_product_rel WHERE user_id = ?", id)
+        flash[:notice] = "användare raderad"
+    end
+
+    # Edits a user
+    #
+    # @param [Integer] username, the new users username
+    # @param [Integer] pwd, the new users password
+    # @param [Integer] role, the new users role
+    # @param [Integer] id, the users id
+    #
+    # @return [flash] message confirming deletion
+    def edit_user(username, pwd, role, id)
+        db = connect_to_db()
+        pwddigest = BCrypt::Password.create(password)
+        db.execute("UPDATE users SET username=?,pwdigest=?,role=? WHERE id=?", name, pwddigest, role, id)
+        flash[:notice] = "användare redigerad"
+
+    end
+
 end
-
-def fetch_user(username)
-    db = connect_to_db()
-    return db.execute("SELECT * FROM users WHERE username=?", username).first
-end
-
-def fetch_user_id(id)
-    db = connect_to_db()
-    return db.execute("SELECT * FROM users WHERE id = ?", id).first
-end
-
-def fetch_users()
-    db = connect_to_db()
-    return db.execute("SELECT * FROM users")
-end
-
-def insert_purchase(user_id, product_id)
-    db = connect_to_db()
-    db.execute("INSERT INTO user_product_rel (user_id, product_id) VALUES (?, ?)", user_id,  product_id)
-    flash[:notice] = "produkt köpt"
-end
-
-def remove_purchase(id)
-    db = connect_to_db()
-    db.execute("DELETE FROM user_product_rel WHERE id = ?", id)
-    flash[:notice] = "produkt borttagen från purchases"
-end
-
-def delete_user(id)
-    db = connect_to_db()
-    db.execute("DELETE FROM users WHERE id = ?", id)
-    db.execute("DELETE FROM user_product_rel WHERE user_id = ?", id)
-    flash[:notice] = "användare raderad"
-end
-
-def edit_user(username, pwd, role, id)
-    db = connect_to_db()
-    pwddigest = BCrypt::Password.create(password)
-    db.execute("UPDATE users SET username=?,pwdigest=?,role=? WHERE id=?", name, pwddigest, role, id)
-    flash[:notice] = "användare redigerad"
-
-end
-
-
-# def is_purchased(user_id, product_id)
-#     db = connect_to_db()
-#     products = db.execute("SELECT * FROM user_product_rel WHERE user_id = ?", user_id)
-#     products.each do |product|
-#         if product['id'] == product_id
-#             return true
-#         end
-#     end
-#     return false
-# end
